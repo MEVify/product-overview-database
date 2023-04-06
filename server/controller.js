facb5ec2f6715479d4db6b51bf582893d0f62bfd
Brett Tanonaka <tano.brett@gmail.com>
2023-04-06 15:56:18 -0700

    Created controller getAll function

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,15 @@
 const model = require('./model.js');
 const axios = require('axios');
 
+module.exports = {
+  getAll: (req, res) => {
+    axios.get(`/products`,
+    { params:
+      { page: req.query.page || 1,
+        count: req.query.count || 5
+      }
+    }).then(response => {
+      res.status(200).send(response.data);
+    }).catch(err => res.status(404).send(err));
+  }
+}
