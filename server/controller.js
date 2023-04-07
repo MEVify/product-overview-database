0a4ed5c2de44ef3b02236680f8662253689f69b3
Brett Tanonaka <tano.brett@gmail.com>
2023-04-07 11:50:23 -0700

    getProducts request sends back all data during get request

--- server/controller.js
+++ server/controller.js
@@ -1,30 +1,28 @@
-const model = require('./model.js');
-const axios = require('axios');
+const model = require('./model');
 
 module.exports = {
   getAll: (req, res) => {
-    console.log('wowowowow');
-    model.getProducts(req.params.product_id, req.params.page, req.params.count)
-    .then(response => {
-      res.status(200).send(response.data);
-    }).catch(err => res.status(500).send(err));
+    model.getProducts(req.query.page, req.query.count)
+      .then((response) => {
+        res.status(200).send(response);
+      }).catch((err) => res.status(500).send(err));
   },
 
   getOne: (req, res) => {
     model.getOne()
-    .then(response => res.status(200).send(response.data))
-    .catch(err => res.status(500).send(err));
+      .then((response) => res.status(200).send(response.data))
+      .catch((err) => res.status(500).send(err));
   },
 
   getStyles: (req, res) => {
     model.getStyles()
-    .then(response => res.status(200).send(response.data))
-    .catch(err => res.status(500).send(err));
+      .then((response) => res.status(200).send(response.data))
+      .catch((err) => res.status(500).send(err));
   },
 
   getRelated: (req, res) => {
     model.getRelated()
-    .then(response => res.status(200).send(response.data))
-    .catch(err => res.status(500).send(err));
-  }
-}
+      .then((response) => res.status(200).send(response.data))
+      .catch((err) => res.status(500).send(err));
+  },
+};
--- server/model.js
+++ server/model.js
@@ -1,24 +1,59 @@
-const db = require('./db')
+const db = require('./db');
 
 module.exports = {
-  getProducts: (productId = 3, page = 2, count = 5) => {
+  getProducts: (page = 1, count = 5) => {
     try {
-      const query = db.many(`SELECT json_agg(
-        SELECT json_build_object(
-        'id', product.id,
-        'name', product.name,
-        'slogan', product.slogan,
-        'description', product.description,
-        'category', product.category,
-        'default_price', product.default_price
-        ))
-        AS products FROM product
-        WHERE id >= ${productId}
-        LIMIT ${count};`)
+      const query = db.many(
+        `SELECT * FROM product
+        OFFSET ${((page - 1) * count)}
+        LIMIT ${count};`,
+      );
       return query;
     }
-    catch(e) {
-      console.log('getProducts failed to get products')
+    catch (err) {
+      console.error(`getProducts: ${err}`);
     }
   },
-}
+
+//   getOne: (productId = 1) => {
+//     try {
+//       const query = db.one(`
+//         SELECT json_build_object(
+//         'id', product.id,
+//         'name', product.name,
+//         'slogan', product.slogan,
+//         'description', product.description,
+//         'category', product.category,
+//         'default_price', product.default_price
+//         SELECT json_agg(SELECT json_build_object(
+//           'feature', features.feature,
+//           'value', features.value)
+//           FROM features
+//           WHERE features.product_id = product.id)
+//         )
+//         AS products FROM product
+//         WHERE id >= ${productId}
+//         LIMIT ${count};`)
+//       return query;
+//     }
+//     catch(e) {
+//       console.log('getProducts failed to get products')
+//     }
+//   },
+};
+
+
+// const query = db.many(
+//   `SELECT json_agg(
+//   json_build_object(
+//   'id', id,
+//   'name', name,
+//   'slogan', slogan,
+//   'description', description,
+//   'category', category,
+//   'default_price', default_price
+//   )
+//   )
+//   AS products FROM product
+//   LIMIT ${count};`,
+// );
