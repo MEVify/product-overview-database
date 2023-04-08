97be40d60f92650b6d845f06fe83bf53ae4c7bd0
Brett Tanonaka <tano.brett@gmail.com>
2023-04-08 14:54:36 -0700

    getOne returns specific data for a product

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../server');
 
-describe('Get all products', () => {
+describe('getAll', () => {
   it('Should return data as an array with default count of 5', async () => {
     const response = await request(app).get('/products');
     expect(response.statusCode).toBe(200);
@@ -25,6 +25,44 @@ describe('Get all products', () => {
         description: 'The Pumped Up serves up crisp court style with a modern look. These shoes show off tennis-whites shades and are constructed with a supple leather upper and a classic rubber cupsole.',
         category: 'Kicks',
         default_price: 89,
+      }),
+    );
+  });
+});
+
+describe('getOne', () => {
+  it('Should return data for specified product id', async () => {
+    const productId = 40342;
+    const response = await request(app)
+      .get(`/products/${productId}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: 40342,
+        name: 'Breana Shorts',
+        slogan: 'Voluptatem quam tenetur doloribus.',
+        description: 'Deleniti nostrum eum aspernatur animi qui laborum unde. Rerum quasi cupiditate et maiores repellat illo. Odio laboriosam asperiores earum aperiam. Soluta ipsa doloribus officiis laborum nulla provident deleniti aut error. Illo id vel id et doloremque consequatur vel.',
+        category: 'Shorts',
+        default_price: 631,
+        features: [
+          {
+            feature: 'Lens',
+            value: 'Ultrasheen Basic',
+          },
+          {
+            feature: 'Fabric',
+            value: 'Silk',
+          },
+          {
+            feature: 'Frame',
+            value: 'AllLight Composition Resin',
+          },
+          {
+            feature: 'Material',
+            value: 'Control Support Bridge',
+          },
+        ],
       }),
     );
   });
