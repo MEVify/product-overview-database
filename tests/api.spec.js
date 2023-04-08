b317d6773446cd14778b9bf5d9344a7752c70c3e
Brett Tanonaka <tano.brett@gmail.com>
2023-04-08 13:55:05 -0700

    getAll test looks for real data

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -10,16 +10,35 @@ describe('Get all products', () => {
   });
 
   it('Should return data as an object', async () => {
-    const response = await request(app).get('/products');
-    expect(response.body[1]).toEqual(
+    const response = await request(app)
+      .get('/products')
+      .query({
+        page: 2,
+        count: 5,
+      });
+
+    expect(response.body[0]).toEqual(
       expect.objectContaining({
-        id: expect.any(Number),
-        name: expect.any(String),
-        slogan: expect.any(String),
-        description: expect.any(String),
-        category: expect.any(String),
-        default_price: expect.any(Number),
+        id: 6,
+        name: 'Pumped Up Kicks',
+        slogan: 'Faster than a just about anything',
+        description: 'The Pumped Up serves up crisp court style with a modern look. These shoes show off tennis-whites shades and are constructed with a supple leather upper and a classic rubber cupsole.',
+        category: 'Kicks',
+        default_price: 89,
       }),
     );
   });
 });
+
+
+
+// expect(response.body[1]).toEqual(
+//   expect.objectContaining({
+//     id: expect.any(Number),
+//     name: expect.any(String),
+//     slogan: expect.any(String),
+//     description: expect.any(String),
+//     category: expect.any(String),
+//     default_price: expect.any(Number),
+//   }),
+// );
