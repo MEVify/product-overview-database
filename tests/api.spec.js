15f2df03b43959892c6339b45bc08527afa6a5d4
Brett Tanonaka <tano.brett@gmail.com>
2023-04-08 15:27:10 -0700

    Acheived 93% test coverage, ignoring coverage files in commit

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -66,6 +66,13 @@ describe('getOne', () => {
       }),
     );
   });
+
+  it('Should return data for specified product id', async () => {
+    const productId = 123456789;
+    const response = await request(app)
+      .get(`/products/${productId}`)
+    expect(response.statusCode).toBe(500);
+  });
 });
 
 describe('getStyles', () => {
