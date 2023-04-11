0742c48a35e517cb3bc2bb72572f43f86324b924
Brett Tanonaka <tano.brett@gmail.com>
2023-04-11 15:58:53 -0700

    rearranged connection in db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const pgp = require('pg-promise')();
 
 const connection = {
+  user: 'ubuntu',
   host: 'ec2-52-21-104-6.compute-1.amazonaws.com',
   database: 'product_overview',
-  user: 'ubuntu',
   password: '',
   port: 5432,
 };
