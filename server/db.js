8c3ae705e86f6d6301774f70b1dc295f373c96a8
Brett Tanonaka <tano.brett@gmail.com>
2023-04-11 15:41:53 -0700

    Changed db.js user to ubuntu

--- server/db.js
+++ server/db.js
@@ -3,8 +3,9 @@ const pgp = require('pg-promise')();
 const connection = {
   host: 'ec2-52-21-104-6.compute-1.amazonaws.com',
   database: 'product_overview',
-  user: 'brett',
+  user: 'ubuntu',
   password: '',
+  port: 5432,
 };
 
 const db = pgp(connection);
