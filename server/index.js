d9855e43c5c3393bf7685dd52211af8437f3e0fc
Brett Tanonaka <tano.brett@gmail.com>
2023-04-12 11:26:16 -0700

    Added route to payload file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const cors = require('cors');
 const router = require('./router');
+const payload = require('../k6ProductIds.js');
 
 const app = express();
 
@@ -10,9 +11,14 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/products', router);
+
+// Verify for loader.io
 app.get('/loaderio-270ab75e2947899575ebf3f6990d9f8f', (req, res) => (
   res.send('loaderio-270ab75e2947899575ebf3f6990d9f8f')
 ));
+
+// Payload for loader.io
+app.get('/load-file', (req, res) => { res.send(payload); });
 
 const PORT = process.env.PORT || 3230;
 
