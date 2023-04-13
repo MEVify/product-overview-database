942d600c9ee26863b401bb8face7c6020f5fbb6b
Brett Tanonaka <tano.brett@gmail.com>
2023-04-13 16:10:03 -0700

    Changed database host address

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 
 const connection = {
   user: 'brett',
-  host: 'ec2-3-82-129-44.compute-1.amazonaws.com',
+  host: 'ec2-3-95-168-61.compute-1.amazonaws.com',
   database: 'product_overview',
   password: 'password',
   port: 5432,
